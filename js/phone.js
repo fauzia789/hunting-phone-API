13c3339d398ded2db98cfd6d118b8157ed4f753e
fauzia789 <fauziafaiza789@gmail.com>
2024-03-03 23:28:54 +0700

    js fixed

--- js/phone.js
+++ js/phone.js
@@ -1,16 +1,16 @@
-const loadPhone = async(searchText) =>{
+const loadPhone = async(searchText,isShowAll) =>{
     const res = await fetch(`
     https://openapi.programming-hero.com/api/phones?search=${searchText}`);
     const data = await res.json();
     const phones =data.data;
-   displaPhones(phones);
+   displaPhones(phones,isShowAll);
 }
 
-const displaPhones = phones =>{
+const displaPhones = (phones,isShowAll) =>{
     const phoneContainer = document.getElementById('card-container');
     phoneContainer.textContent='';
     const showAll = document.getElementById('show-All');
-if(phones.length > 12){
+if(phones.length > 12 && !isShowAll ){
 showAll.classList.remove('hidden');
 }
 else{
@@ -18,10 +18,14 @@ else{
 }
 
 
+ if(!isShowAll){
     phones=phones.slice(0,12);
+    
+ }
+
     // console.log(phones);
     phones.forEach(phone =>{
-        console.log(phone);
+        // console.log(phone);
 
         const phoneCard =document.createElement('div');
         phoneCard.classList=`card p-4 bg-base-100 shadow-xl `;
@@ -30,8 +34,8 @@ else{
         <div class="card-body">
           <h2 class="card-title">${phone.phone_name}</h2>
           <p>If a dog chews shoes whose shoes does he choose?</p>
-          <div class="card-actions justify-end">
-            <button class="btn btn-primary">Buy Now</button>
+          <div class="card-actions justify-center">
+            <button onclick="showDetails('${phone.slug}')" class="btn btn-primary">Show details</button>
           </div>
         </div>
         `;
@@ -39,8 +43,35 @@ else{
         
 
     })
+    loadSpinner(false)
 }
 
+const showDetails =async(id) =>{
+   const res =await fetch(`https://openapi.programming-hero.com/api/phone/${id}`);
+   const data =await res.json();
+  const phone =data.data;
+   phonDetail(phone);
+}
+
+const phonDetail =(phone)=>{
+    console.log(phone);
+    const phoneName =document.getElementById('phone-detail');
+    phoneName.innerText = phone.name;
+    const showContaoiner =document.getElementById('show-detail-container');
+    showContaoiner.innerHTML=`
+    <figure><img src="${phone.image}" /></figure>
+    <div class="card-body">
+      <h2 class="card-title">${phone.brand}</h2>
+      <p>If a dog chews shoes whose shoes does he choose?</p>
+      <div class="card-actions justify-center">
+        <button onclick="showDetails('${phone.mainFeatures
+            }')" class="btn btn-primary">Show details</button>
+      </div>
+    </div>
+    `;
+    show_modal.showModal();
+
+}
 
 // const handleSearch2 =()=>{
 //     const search = document.getElementById('seatrch-filed2');
@@ -49,10 +80,24 @@ else{
     
 // }
 
-const handleSearch =()=>{
+const handleSearch =(isShowAll)=>{
+    loadSpinner(true);
     const secrchField =  document.getElementById('seatrch-filed');
     const seaxcxrhValue =secrchField.value;
 
-    loadPhone(seaxcxrhValue);
+    loadPhone(seaxcxrhValue ,isShowAll);
 }
 
+const loadSpinner =(isLoading)=>{
+    const loadSpinner =document.getElementById('load-spinner');
+  if(isLoading){
+    loadSpinner.classList.remove('hidden');
+  }
+  else{
+    loadSpinner.classList.add('hidden');
+  }
+}
+
+const isShowALl =()=>{
+    handleSearch(true);
+}
